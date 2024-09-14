954692d047c58e33f3cc01eb4aaed5403e40ba15
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-09-15 00:59:41 +0200

    free_stack cleand and corrected

--- src/push_swap/sanity.c
+++ src/push_swap/sanity.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 18:56:17 by tomek             #+#    #+#             */
-/*   Updated: 2024/09/12 00:33:12 by tomek            ###   ########.fr       */
+/*   Updated: 2024/09/15 00:24:48 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +28,20 @@ void	free_ssplit(char **ssplit)
 	free(ssplit);
 }
 
-void	free_stack(t_stack *stack)
+void	free_stack(t_node **stack)
 {
 	t_node	*current;
 	t_node	*next_node;
 
 	if (stack == NULL)
 		return ;
-	current = stack->head;
+	current = *stack;
 	while (current != NULL)
 	{
 		next_node = current->next;
+		current->data = 0;
 		free(current);
 		current = next_node;
 	}
-	free(stack);
+	*stack = NULL;
 }
