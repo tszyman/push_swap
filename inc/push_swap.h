7468af5ccd03a7c11d422b247716519184d9d97d
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-09-16 01:22:31 +0200

    added new fields to t_node struct and update new function definitions.

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 16:09:32 by tomek             #+#    #+#             */
-/*   Updated: 2024/09/16 00:34:51 by tomek            ###   ########.fr       */
+/*   Updated: 2024/09/16 01:19:21 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,10 @@ typedef struct s_node
 {
 	int				data;
 	int				index;
+	int				push_cost;
 	bool			above_median;
+	bool			cheapest;
+	struct s_node	*target;
 	struct s_node	*prev;
 	struct s_node	*next;
 }	t_node;
@@ -49,10 +52,12 @@ int		stack_len(t_node *stack);
 int		stack_sorted(t_node *stack);
 t_node	*find_last(t_node *stack);
 t_node	*find_max(t_node *stack);
+t_node	*get_cheapest(t_node *stack);
 
 //prep_nodes
 void	prep_nodes_a(t_node *stack_a, t_node *stack_b);
 void	current_index(t_node *stack);
+void	set_cheapest(t_node *stack);
 
 //actions
 //		swap
@@ -72,6 +77,7 @@ void	pa(t_node **stack_a, t_node **stack_b, bool print);
 void	pb(t_node **stack_b, t_node **stack_a, bool print);
 //		algorithms
 void	sort_three(t_node **stack_a);
+void	sort_stacks_turk(t_node **stack_a, t_node **stack_b);
 
 //sort
 
