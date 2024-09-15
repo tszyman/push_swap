d90a93ffa7efde1d62dfc57eabb08e620684e762
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-09-15 23:16:45 +0200

    find_max function implemented

--- src/push_swap/stack_utils.c
+++ src/push_swap/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 19:36:22 by tomek             #+#    #+#             */
-/*   Updated: 2024/09/15 20:57:58 by tomek            ###   ########.fr       */
+/*   Updated: 2024/09/15 22:44:22 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_node	*find_last(t_node *stack)
 {
-	if(!stack)
+	if (!stack)
 		return (0);
 	while (stack->next)
 		stack = stack->next;
@@ -24,8 +24,8 @@ t_node	*find_last(t_node *stack)
 int	stack_len(t_node *stack)
 {
 	int	len;
-	
-	if(!stack)
+
+	if (!stack)
 		return (0);
 	len = 0;
 	while (stack)
@@ -38,7 +38,7 @@ int	stack_len(t_node *stack)
 
 int	stack_sorted(t_node *stack)
 {
-	if(!stack)
+	if (!stack)
 		return (0);
 	while (stack->next)
 	{
@@ -47,6 +47,26 @@ int	stack_sorted(t_node *stack)
 		stack = stack->next;
 	}
 	return (1);
+}
+
+t_node	*find_max(t_node *stack)
+{
+	t_node	*max_node;
+	long	max_val;
+
+	if (!stack)
+		return (0);
+	max_val = LONG_MIN;
+	while (stack)
+	{
+		if (stack->data > max_val)
+		{
+			max_val = stack->data;
+			max_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (max_node);
 }
 
 void	print_stack(t_node	*stack)
